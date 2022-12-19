2b87a8fb9d54c299161727723d0df57c29ce4380
Nikolas94 <nikolasva1994@gmail.com>
2022-12-19 19:39:08 +0100

    Revert "settings updated with os.getenv"
    This reverts commit 63a153018bc9ee55e9180158fdb06323b887edb2.

--- ziaboga_back/settings.py
+++ ziaboga_back/settings.py
@@ -82,7 +82,8 @@ WSGI_APPLICATION = 'ziaboga_back.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-if os.getenv("DEVELOPMENT_MODE", "True") == "True":
+
+if os.getenv("DEVELOPMENT_MODE", "False") == "True":
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
@@ -131,7 +132,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
