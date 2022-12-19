63a153018bc9ee55e9180158fdb06323b887edb2
Nikolas94 <nikolasva1994@gmail.com>
2022-12-19 19:36:44 +0100

    settings updated with os.getenv

--- ziaboga_back/settings.py
+++ ziaboga_back/settings.py
@@ -82,8 +82,7 @@ WSGI_APPLICATION = 'ziaboga_back.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
-if os.getenv("DEVELOPMENT_MODE", "False") == "True":
+if os.getenv("DEVELOPMENT_MODE", "True") == "True":
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
@@ -132,6 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
